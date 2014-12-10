dc898ae71bbf1fca44c88833a2a7f1e703012333
Basheer Tome <basheer@basheertome.com>
2014-12-10 10:22:25 -0500

    Changed random generator a bit

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -177,9 +177,11 @@ $(document).ready(function() {
 	for (var i=1; i<27; i++) {
 		for (var j=1; j<17; j++) {
 			if (Math.round((Math.random())) > 0) {
-				$('.sequences ul:nth-child(' + i + ') li:nth-child(' + j + ')').css({'opacity': .125});
-				$('.sequences ul:nth-child(' + i + ') li:nth-child(' + j + ')').css({'opacity': .125});
-			}
+			if (Math.round((Math.random())) > 0) {
+			if (Math.round((Math.random())) > 0) {
+				$('.sequences ul:nth-child(' + i + ') li:nth-child(' + j + ')').css({'opacity': 1});
+				$('.sequences ul:nth-child(' + i + ') li:nth-child(' + j + ')').css({'opacity': 1});
+			}}}
 		}
 	}
 
