4dd47194609b556d968b4fa647b084c45a941dd8
Basheer Tome <basheer@basheertome.com>
2014-12-09 21:49:40 -0500

    Fixed flags for animations finally. Added some playstate tests in jquery

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -2,16 +2,55 @@ $(document).ready(function(){
 	for (var i=0; i<27; i++) {
 		for (var j=0; j<17; j++) {
 			if (Math.round((Math.random())) > 0) {
-				$('.sequences ul:nth-child(' + i + ') li:nth-child(' + j + ')').css({'opacity': Math.random()/5.0 + .15});
-				$('.sequences ul:nth-child(' + i + ') li:nth-child(' + j + ')').css({'opacity': Math.random()/5.0 + .15});
+				$('.sequences ul:nth-child(' + i + ') li:nth-child(' + j + ')').css({'opacity': .15});
+				$('.sequences ul:nth-child(' + i + ') li:nth-child(' + j + ')').css({'opacity': .15});
 			}
 		}
 	}
 
 	$(document).keydown(function(e) {
 		console.log(e.keyCode);
-		if(e.keyCode == 32) {
-			$('.sequences ul li').css({opacity: 0.15});
+		if (e.keyCode == 46 || e.keyCode == 8) {
+			// Backspace / Delete
+			e.preventDefault();
+			$('.sequences ul li').css({
+				'opacity': 0.15
+			});
+			playState('paused');
+		}
+		if (e.keyCode == 32) {
+			// Spacebar
+			if (playState() === 'running') {
+				playState('paused');
+			} else {
+				playState('running');
+			}
+		}
+		if (e.keyCode == 38) {
+			// Up Arrow
+		}
+		if (e.keyCode == 40) {
+			// Down Arrow
 		}
 	});
 });
+
+function playState(state) {
+	if (state === 'running') {
+		$('.sequences ul li').css({
+			'-webkit-animation-play-state': 'running',
+			'-moz-animation-play-state': 'running',
+			'animation-play-state': 'running'
+		});
+		return state;
+	} else if (state === 'paused') {
+		$('.sequences ul li').css({
+			'-webkit-animation-play-state': 'paused',
+			'-moz-animation-play-state': 'paused',
+			'animation-play-state': 'paused'
+		});
+		return state;
+	} else {
+		return $('.sequences ul li').css('animation-play-state');
+	}
+}
