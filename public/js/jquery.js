cd21bbaf8b6863353ab126581a426f25bea0794e
Donald Derek <donaldderek.haddad@gmail.com>
2014-12-10 10:14:37 -0500

    let there be sound

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -1,5 +1,179 @@
+var tracks = [
+{
+	"id": 0,
+	"name": "a",
+	"filename": "arp-c2.mp3"
+},
+{
+	"id": 1,
+	"name": "b",
+	"filename": "arp-c3.mp3"
+},
+{
+	"id": 2,
+	"name": "c",
+	"filename": "base-f4.mp3"
+},
+{
+	"id": 3,
+	"name": "d",
+	"filename": "base-f5.mp3"
+},
+{
+	"id": 4,
+	"name": "e",
+	"filename": "bell.mp3"
+},
+{
+	"id": 5,
+	"name": "f",
+	"filename": "bongo-hi.mp3"
+},
+{
+	"id": 6,
+	"name": "g",
+	"filename": "bongo-low.mp3"
+},
+{
+	"id": 7,
+	"name": "h",
+	"filename": "chimes.mp3"
+},
+{
+	"id": 8,
+	"name": "i",
+	"filename": "conga-hi.mp3"
+},
+{
+	"id": 9,
+	"name": "j",
+	"filename": "conga-low.mp3"
+},
+{
+	"id": 10,
+	"name": "k",
+	"filename": "cowbell.mp3"
+},
+{
+	"id": 11,
+	"name": "l",
+	"filename": "cyan.mp3"
+},
+{
+	"id": 12,
+	"name": "m",
+	"filename": "hihat-closed.mp3"
+},
+{
+	"id": 13,
+	"name": "n",
+	"filename": "hihat-open.mp3"
+},
+{
+	"id": 14,
+	"name": "o",
+	"filename": "kick.mp3"
+},
+{
+	"id": 15,
+	"name": "p",
+	"filename": "lofi.mp3"
+},
+{
+	"id": 16,
+	"name": "q",
+	"filename": "piano.mp3"
+},
+{
+	"id": 17,
+	"name": "r",
+	"filename": "rimshot.mp3"
+},
+{
+	"id": 18,
+	"name": "s",
+	"filename": "saw-c4.mp3"
+},
+{
+	"id": 19,
+	"name": "t",
+	"filename": "saw-c5.mp3"
+},
+{
+	"id": 20,
+	"name": "u",
+	"filename": "shaker.mp3"
+},
+{
+	"id": 21,
+	"name": "v",
+	"filename": "snare.mp3"
+},
+{
+	"id": 22,
+	"name": "w",
+	"filename": "tambourin.mp3"
+},
+{
+	"id": 23,
+	"name": "x",
+	"filename": "wood-hi.mp3"
+},
+{
+	"id": 24,
+	"name": "y",
+	"filename": "wood-low.mp3"
+},
+{
+	"id": 25,
+	"name": "z",
+	"filename": "wub.mp3"
+}
+]
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+var samples = [];
+var loader=0;
+
+tracks.forEach(function(track, i, arr){
+	loadSample('samples/'+track.filename);
+})
+
+function loadSample(url) {
+	var request = new XMLHttpRequest();
+	request.open('GET', url, true);
+	request.responseType = 'arraybuffer';
+
+	// Decode asynchronously and push to samples
+	request.onload = function() {
+		context.decodeAudioData(request.response, function(buffer) {
+			samples.push(buffer);
+			incLoader(loader++)
+		}, function(err){});
+	}
+	request.send();
+
+}
+
+function incLoader(loader){
+	if(loader == tracks.length-1)
+		return samplesLoaded(samples);
+	}
+
+function playSound(buffer) {
+	var source = context.createBufferSource();
+	source.buffer = buffer;
+	source.connect(context.destination);
+	source.start(0);
+}
+
+function samplesLoaded(samples){
+	console.log(samples);
+}
+
 $(document).ready(function() {
-	// Seeding random "data" so that it doesn't look bad while I'm coding	
+	// Seeding random "data" so that it doesn't look bad while I'm coding
 	for (var i=1; i<27; i++) {
 		for (var j=1; j<17; j++) {
 			if (Math.round((Math.random())) > 0) {
@@ -178,16 +352,18 @@ var tempo = 120;
 // Basic functions for pulse
 
 function pulse(object) {
-	if (object.css('opacity') > .125) {
-		object.addClass('pulse').on('webkitAnimationEnd animationend', function() {
-			$(this).removeClass('pulse').off('webkitAnimationEnd animationend');
-		});
-	}
+	object.addClass('pulse').on('webkitAnimationEnd animationend', function() {
+		$(this).removeClass('pulse').off('webkitAnimationEnd animationend');
+	});
 }
 
 function row() {
 	for (var i=1; i<27; i++) {
-		pulse($('.sequences ul:nth-child(' + i + ') li:nth-child(' + time + ')'));
+		var object = $('.sequences ul:nth-child(' + i + ') li:nth-child(' + time + ')')
+		if (object.css('opacity') > .125) {
+			pulse(object);
+			playSound(samples[i-1]);
+		}
 	}
 }
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,141 +1,200 @@
-var audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-function Sound(source, level) {
-	var that = this;
-	that.source = source;
-	that.buffer = null;
-	that.isLoaded = false;
-
-	var getSound = new XMLHttpRequest();
-	getSound.open("GET",that.source,true);
-	getSound.responseType = "arraybuffer";
-	getSound.onload = function() {
-		audioContext.decodeAudioData(getSound.response,function(buffer) {
-			that.buffer = buffer;
-			that.isLoaded = true;
-		});
-	};
-	getSound.send();
-}
-
-Sound.prototype.play = function(){
-
-	if(this.isLoaded === true) {
-		isOn = true;
-		playSound = audioContext.createBufferSource();
-		playSound.buffer = this.buffer;
-		playSound.connect(audioContext.destination);
-		playSound.start(0);
-	}
-};
-
-var tracks = [
+/*var tracks = [
 {
-	"name": "a",
-	"filename": "arp-c2.mp3"
+"id": 0,
+"name": "a",
+"filename": "arp-c2.mp3"
 },
 {
-	"name": "b",
-	"filename": "arp-c3.mp3"
+"id": 1,
+"name": "b",
+"filename": "arp-c3.mp3"
 },
 {
-	"name": "c",
-	"filename": "base-f4.mp3"
+"id": 2,
+"name": "c",
+"filename": "base-f4.mp3"
 },
 {
-	"name": "d",
-	"filename": "base-f5.mp3"
+"id": 3,
+"name": "d",
+"filename": "base-f5.mp3"
 },
 {
-	"name": "e",
-	"filename": "bell.mp3"
+"id": 4,
+"name": "e",
+"filename": "bell.mp3"
 },
 {
-	"name": "f",
-	"filename": "bongo-hi.mp3"
+"id": 5,
+"name": "f",
+"filename": "bongo-hi.mp3"
 },
 {
-	"name": "g",
-	"filename": "bongo-low.mp3"
+"id": 6,
+"name": "g",
+"filename": "bongo-low.mp3"
 },
 {
-	"name": "h",
-	"filename": "chimes.mp3"
+"id": 7,
+"name": "h",
+"filename": "chimes.mp3"
 },
 {
-	"name": "i",
-	"filename": "conga-hi.mp3"
+"id": 8,
+"name": "i",
+"filename": "conga-hi.mp3"
 },
 {
-	"name": "j",
-	"filename": "conga-low.mp3"
+"id": 9,
+"name": "j",
+"filename": "conga-low.mp3"
 },
 {
-	"name": "k",
-	"filename": "cowbell.mp3"
+"id": 10,
+"name": "k",
+"filename": "cowbell.mp3"
 },
 {
-	"name": "l",
-	"filename": "cyan.mp3"
+"id": 11,
+"name": "l",
+"filename": "cyan.mp3"
 },
 {
-	"name": "m",
-	"filename": "hihat-closed.mp3"
+"id": 12,
+"name": "m",
+"filename": "hihat-closed.mp3"
 },
 {
-	"name": "n",
-	"filename": "hihat-open.mp3"
+"id": 13,
+"name": "n",
+"filename": "hihat-open.mp3"
 },
 {
-	"name": "o",
-	"filename": "kick.mp3"
+"id": 14,
+"name": "o",
+"filename": "kick.mp3"
 },
 {
-	"name": "p",
-	"filename": "lofi.mp3"
+"id": 15,
+"name": "p",
+"filename": "lofi.mp3"
 },
 {
-	"name": "q",
-	"filename": "piano.mp3"
+"id": 16,
+"name": "q",
+"filename": "piano.mp3"
 },
 {
-	"name": "r",
-	"filename": "rimshot.mp3"
+"id": 17,
+"name": "r",
+"filename": "rimshot.mp3"
 },
 {
-	"name": "s",
-	"filename": "saw-c4.mp3"
+"id": 18,
+"name": "s",
+"filename": "saw-c4.mp3"
 },
 {
-	"name": "t",
-	"filename": "saw-c5.mp3"
+"id": 19,
+"name": "t",
+"filename": "saw-c5.mp3"
 },
 {
-	"name": "u",
-	"filename": "shaker.mp3"
+"id": 20,
+"name": "u",
+"filename": "shaker.mp3"
 },
 {
-	"name": "v",
-	"filename": "snare.mp3"
+"id": 21,
+"name": "v",
+"filename": "snare.mp3"
 },
 {
-	"name": "w",
-	"filename": "tambourin.mp3"
+"id": 22,
+"name": "w",
+"filename": "tambourin.mp3"
 },
 {
-	"name": "x",
-	"filename": "wood-hi.mp3"
+"id": 23,
+"name": "x",
+"filename": "wood-hi.mp3"
 },
 {
-	"name": "y",
-	"filename": "wood-low.mp3"
+"id": 24,
+"name": "y",
+"filename": "wood-low.mp3"
 },
 {
-	"name": "z",
-	"filename": "wub.mp3"
+"id": 25,
+"name": "z",
+"filename": "wub.mp3"
 }
 ]
 
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+var samples = [];
+var loader=0;
+
+tracks.forEach(function(track, i, arr){
+loadSample('samples/'+track.filename);
+})
+
+function loadSample(url) {
+var request = new XMLHttpRequest();
+request.open('GET', url, true);
+request.responseType = 'arraybuffer';
+
+// Decode asynchronously and push to samples
+request.onload = function() {
+context.decodeAudioData(request.response, function(buffer) {
+samples.push(buffer);
+incLoader(loader++)
+}, function(err){});
+}
+request.send();
+
+}
+
+function incLoader(loader){
+if(loader == tracks.length-1)
+return samplesLoaded(samples);
+}
+
+function playSound(buffer, time) {
+var source = context.createBufferSource();
+source.buffer = buffer;
+source.connect(context.destination);
+source.start(time);
+}
+
+function samplesLoaded(samples){
+var kick = samples[14];
+var snare = samples[14];
+var hihat = samples[13];
+//playSound(kick,0)
+//playSound(kick,0.5)
+//playSound(kick,1)
+playSound(kick,0)
+playSound(kick,1)
+
+for (var bar = 0; bar < 2; bar++) {
+var time = startTime + bar * 8 * eighthNoteTime;
+// Play the bass (kick) drum on beats 1, 5
+playSound(kick, time);
+playSound(kick, time + 4 * eighthNoteTime);
+
+// Play the snare drum on beats 3, 7
+playSound(snare, time + 2 * eighthNoteTime);
+playSound(snare, time + 6 * eighthNoteTime);
+
+// Play the hi-hat every eighth note.
+for (var i = 0; i < 8; ++i) {
+playSound(hihat, time + i * eighthNoteTime);
+}
+}
+}
+
 $(document).ready(function(){
-	console.log(tracks[0].name);
-});
+
+});*/
