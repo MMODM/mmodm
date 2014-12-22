f439855e51bb1be3ed3244cbbff0f26fa2f9eb37
Donald Derek <donaldderek.haddad@gmail.com>
2014-12-22 16:45:22 -0500

    change server cluster architecture

--- app.js
+++ app.js
@@ -12,11 +12,132 @@ var path = require('path');
 var config = require('./config')[process.env.ENVAR || 'dev'];
 var twitter = require('ntwitter');
 var fs = require('fs');
-var cpuCount = require('os').cpus().length;
 var cluster = require('cluster');
-var redis = require('socket.io-redis');
-var sio = require('socket.io');
 var ioe = require('socket.io-emitter')({ host: '127.0.0.1', port: 6379 });
+
+var redis = require('redis');
+var redisAdapter = require('socket.io-redis');
+var sticky = require('sticky-session');
+
+
+var port = process.env.PORT || 3000;
+var workers = process.env.WORKERS || require('os').cpus().length;
+var app = express();
+
+sticky(workers, function() {
+
+    // This code will be executed only in slave workers
+    var server = http.createServer(app);
+
+    var io = require('socket.io')(server);
+
+    // configure socket.io to use redis adapter
+    addRedisAdapter(io);
+
+    // configure socket.io to respond to certain events
+    addIOEventHandlers(io);
+
+    return server;
+
+}).listen(port, function() {
+
+    // this code is executed in both slaves and master
+    console.log('server started on port '+port+'. process id = '+process.pid);
+
+});
+
+mongoose.connect(config.db);
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.compress());
+app.use(express.favicon());
+app.use(express.cookieParser());
+app.use(express.session({ secret: config.session_secret }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(function (req, res, next) {
+    res.header("X-powered-by", "The Force")
+    next()
+})
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public'),{ maxAge: 2629800000 }));
+
+//Routes
+app.get('/', routes.index);
+
+app.get('/tweet/:msg', routes.tweet)
+
+app.get('/save/:longurl', function(req, res){
+    controller.saveState(req.params.longurl, function(err, id){
+        res.send(id);
+    })
+})
+
+app.get('/sm/:shorturl', function(req, res){
+    controller.findState(req.params.shorturl, function(err, longUrl){
+        res.redirect('/#'+longUrl);
+    })
+})
+
+app.get('/:room', function(req, res){
+
+    var room  = req.params.room
+    res.render('index',{ title: 'MMODM-'+room, user: req.user, room:room })
+})
+
+app.get('/auth/twitter',
+passport.authenticate('twitter'),
+routes.auth);
+
+app.get('/auth/twitter/callback',
+passport.authenticate('twitter', { failureRedirect: '/login' }),
+routes.auth_cb);
+
+app.get('/logout', routes.logout);
+
+//Middlewear
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/')
+}
+
+function emitKeys(users,keystrokes, room){
+    if(users.length >= 0){
+        users.forEach(function(s, i, arr){
+            s.emit('keys', {keys:keystrokes,room:room});
+        })
+    }
+}
+
+function addRedisAdapter(io) {
+    var redisUrl = process.env.REDISTOGO_URL || 'redis://127.0.0.1:6379';
+    var redisOptions = require('parse-redis-url')(redis).parse(redisUrl);
+    var pub = redis.createClient(redisOptions.port, redisOptions.host, {
+        detect_buffers: true,
+        auth_pass: redisOptions.password
+    });
+    var sub = redis.createClient(redisOptions.port, redisOptions.host, {
+        detect_buffers: true,
+        auth_pass: redisOptions.password
+    });
+
+    io.adapter(redisAdapter({
+        pubClient: pub,
+        subClient: sub
+    }));
+    console.log('Redis adapter started with url: ' + redisUrl);
+};
+
+function addIOEventHandlers(io) {
+    io.on('connection', function(socket) {
+        console.log('Connection made. socket.id='+socket.id+' . pid = '+process.pid);
+    });
+};
+
 var twit = new twitter({
     consumer_key: config.consumer_key,
     consumer_secret: config.consumer_secret,
@@ -61,108 +182,4 @@ if (cluster.isMaster) {
             });
         });
 
-        var workers = [];
-        // Helper function for spawning worker at index 'i'.
-        var spawn = function(i) {
-            workers[i] = cluster.fork();
-            workers[i].on('exit', function(worker, code, signal) {
-                console.log('respawning worker', i);
-                spawn(i);
-            });
-        };
-
-        for (var i = 0; i < cpuCount; i++) {
-            spawn(i);
-        }
-
-        var worker_index = function(ip, len) {
-            var s = '';
-            for (var i = 0, _len = ip.length; i < _len; i++) {
-                if (ip[i] !== '.') {
-                    s += ip[i];
-                }
-            }
-            return Number(s) % len;
-        };
-
-        var app = express();
-        var http = require('http').Server(app);
-        var io = require('socket.io')(http);
-        io.adapter(redis({ host: 'localhost', port: 6379 }));
-        mongoose.connect(config.db);
-        app.set('port', process.env.PORT || 3000);
-        app.set('views', __dirname + '/views');
-        app.set('view engine', 'jade');
-        app.use(express.compress());
-        app.use(express.favicon());
-        app.use(express.cookieParser());
-        app.use(express.session({ secret: config.session_secret }));
-        app.use(passport.initialize());
-        app.use(passport.session());
-        app.use(express.logger('dev'));
-        app.use(express.bodyParser());
-        app.use(express.methodOverride());
-        app.use(function (req, res, next) {
-            res.header("X-powered-by", "The Force")
-            next()
-        })
-        app.use(app.router);
-        app.use(express.static(path.join(__dirname, 'public'),{ maxAge: 2629800000 }));
-
-        //Routes
-        app.get('/', routes.index);
-
-        app.get('/tweet/:msg', routes.tweet)
-
-        app.get('/save/:longurl', function(req, res){
-            controller.saveState(req.params.longurl, function(err, id){
-                res.send(id);
-            })
-        })
-
-        app.get('/sm/:shorturl', function(req, res){
-            controller.findState(req.params.shorturl, function(err, longUrl){
-                res.redirect('/#'+longUrl);
-            })
-        })
-
-        app.get('/:room', function(req, res){
-
-            var room  = req.params.room
-            res.render('index',{ title: 'MMODM-'+room, user: req.user, room:room })
-        })
-
-        app.get('/auth/twitter',
-        passport.authenticate('twitter'),
-        routes.auth);
-
-        app.get('/auth/twitter/callback',
-        passport.authenticate('twitter', { failureRedirect: '/login' }),
-        routes.auth_cb);
-
-        app.get('/logout', routes.logout);
-
-        //Middlewear
-        function ensureAuthenticated(req, res, next) {
-            if (req.isAuthenticated()) { return next(); }
-            res.redirect('/')
-        }
-
-        function emitKeys(users,keystrokes, room){
-            if(users.length >= 0){
-                users.forEach(function(s, i, arr){
-                    s.emit('keys', {keys:keystrokes,room:room});
-                })
-            }
-        }
-        http.listen(app.get('port'), function(){
-            console.log('[' + process.pid + '] running MMODM server on port ' + app.get('port') +' with '+cpuCount+' horsemen');
-        });
-    }
-    else{
-        var app = new express();
-        var server = app.listen(0, 'localhost'),
-        io = sio(server);
-        console.log('[' + process.pid + '] is running ws server.')
-        io.adapter(redis({ host: 'localhost', port: 6379 }));
-    }
+}
